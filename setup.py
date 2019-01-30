965d75f6c4e37f63c40f18cf422b22d441494446
Darin Morrison <dmorrison@newengen.com>
2019-01-29 16:47:20 -0800

    fix name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cnn-text-classification-tf",
+    name="cnn_text_classification_tf",
     version="0.0.1",
     description="CNN Text Classification with tensorflow",
     packages=find_packages(exclude=("tests", "docs")),
